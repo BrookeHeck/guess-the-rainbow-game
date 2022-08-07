c3e0272b176980a14842a4eb4b2d783e402993b4
Brooke Heck <b19heck@gmail.com>
2022-08-07 09:24:57 -0700

    working on a dark mode

--- js/colorMode.js
+++ js/colorMode.js
@@ -1,17 +1,48 @@
 'use strict';
 
-const colors = ['rgb(204, 0, 0)', 'rgb(204, 102, 0)', 'rgb(204, 204, 0)', 'rgb(0, 204, 0)', 'rgb(0, 0, 204)', 'rgb(102, 0, 204)', 'rgb(255, 51, 153)', 'rgb(96, 96, 96)'];
+const lightColors = ['rgb(204, 0, 0)', 'rgb(204, 102, 0)', 'rgb(204, 204, 0)', 'rgb(0, 204, 0)', 'rgb(0, 0, 204)', 'rgb(102, 0, 204)', 'rgb(255, 51, 153)', 'rgb(96, 96, 96)'];
 
-function changeHeaderColor() {
+const darkColors = ['rgb(210,39,48)', 'rgb(255,173,0)', 'rgb(224,231,34)', 'rgb(68,214,44)', 'rgb(77, 77, 255)', 'rgb(199,36,177)', 'rgb(219,62,177)', 'rgb(136,139,141)'];
+
+function changeHeaderColor(colorMode) {
   let logo = document.querySelector('h1');
   let counter = 0;
   for (let char of 'Guess the Rainbow') {
     let charSpan = document.createElement('span');
     charSpan.innerHTML = char;
-    if(char !== ' ') charSpan.style.color = colors[counter];
+    if(char !== ' ') charSpan.style.color = colorMode[counter];
     logo.appendChild(charSpan);
     counter++;
-    if(counter === colors.length) counter = 0;
+    if(counter === colorMode.length) counter = 0;
   }
 }
-changeHeaderColor();
+changeHeaderColor(darkColors);
+
+function toggleColorMode(colorMode) {
+  let header = document.querySelector('.navbar');
+  let classes = header.classList;
+  classes.replace('bg-light', 'bg-dark');
+  header.classList = classes;
+
+  let main = document.querySelector('main');
+  main.style.background = 'black';
+
+  let label = document.querySelector('label');
+  label.style.color = 'white';
+}
+toggleColorMode(darkColors);
+
+function updateBoardColors(colorMode) {
+  let guessBoxes = document.querySelectorAll('.oneColor');
+  let colorBoxes = document.querySelectorAll('.colorBox');
+
+  for(let guess of guessBoxes) {
+    if(guess.style.background !== 'rgb(255, 255, 255)') {
+      
+    }
+  }
+}
+
+function updateUserCorrectCombo() {
+
+}
